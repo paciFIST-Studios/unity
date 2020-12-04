f37a2c7bdc68de4ffd9761af7d20729d127488ed
ellielove <ellie@pacifist.studio>
2020-12-04 02:07:50 -0800

    section one complete

---
+++ 1_first_steps/Assets/Introducer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introducer : MonoBehaviour
+{
+	void Start ()
+    {
+        		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
