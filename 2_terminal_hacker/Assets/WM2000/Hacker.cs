a5cceb76c5fa07defe05189deb11da82001c47e1
ellielove <ellie@pacifist.studio>
2020-12-04 03:30:16 -0800

    update

---
+++ 2_terminal_hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    void ShowMainMenu() {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("\n");
+        Terminal.WriteLine("[1] local library");
+        Terminal.WriteLine("[2] police station");
+        Terminal.WriteLine("[3] nasa");
+        Terminal.WriteLine("\n");
+        Terminal.WriteLine("Enter Selection:");
+    }
+
+    void Start () {
+        ShowMainMenu();
+    }
+
+    void Update () {
+
+	}
+}
