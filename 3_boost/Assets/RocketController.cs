9230ad892919c55986013e0ba5a364590cba1154
ellielove <ellie@pacifist.studio>
2020-12-06 00:50:00 -0800

    Update RocketController.cs
    1. can swap between 4 different rocket settings
    
    2. refactor to improve code separation

--- 3_boost/Assets/RocketController.cs
+++ 3_boost/Assets/RocketController.cs
@@ -1,43 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class RocketController : MonoBehaviour
 {
-    private Rigidbody rb;
+    private     Rigidbody    rigidBody;
+    private     AudioSource  audioSource;
+    private     MeshRenderer coneMeshRenderer;
 
-    float boostForce = 500f;
-    float rotationForce = 50f;
+    // used to track start and stop of audio
+    private     bool        playAudio       = false;
+
+    // booster applies this force, per frame, when active (scaled against time.deltaTime)
+    private     float       boostForce      = 500f;
+    // rotation applies this force, per frame, when active (scaled against time.deltaTime)
+    private     float       rotationForce   = 100f;
+
+
+    [System.Serializable]
+    struct RocketSettings
+    {
+        [SerializeField]
+        public float mass;
+        [SerializeField]
+        public float boostForce;
+        [SerializeField]
+        public float rotationForce;
+        [SerializeField]
+        public Material material;
+    }
+
+    [SerializeField]
+    List<RocketSettings> rocketSettings = new List<RocketSettings>();
+
+    private int currentRocketIdx = 0;
 
     private void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        rigidBody = this.GetComponent<Rigidbody>();
+        audioSource = this.GetComponent<AudioSource>();
+
+        // the mesh render of the "cone" object is the specific one that holds color
+        Transform cone = this.transform.Find("cone");
+        coneMeshRenderer = cone.GetComponent<MeshRenderer>();
+
+        SetCurrentRocketIndex(0);
     }
 
     void Update ()
     {
-        ProcessInput();
-	}
-	
-    private void ProcessInput()
+        ProcessDebugInput();
+
+        HandleThrustInput();
+        HandleRotationInput();
+        HandleAudioChanges();
+    }
+
+    private void SetCurrentRocketIndex(int idx)
     {
-        if(Input.GetKey(KeyCode.Space))
-        {
-            // using rocket's coordinate system
-            rb.AddRelativeForce(Vector3.up * boostForce * Time.deltaTime);
-        }
+        Assert.IsTrue(idx < rocketSettings.Count);
+        Assert.IsTrue(idx > 0);
 
-        // left rotate has precedence 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward *  rotationForce * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward *  rotationForce * Time.deltaTime);
-        }
+        boostForce = rocketSettings[idx].boostForce;
+        rotationForce = rocketSettings[idx].rotationForce;
+        coneMeshRenderer.material = rocketSettings[idx].material;
+    }
 
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+    private void ProcessDebugInput()
+    {
+        // update boost forces
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             boostForce += 50f;
         }
@@ -45,6 +78,73 @@ public class RocketController : MonoBehaviour
         {
             boostForce -= 50f;
         }
+
+        // switch between different rocket configurations
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentRocketIdx--;
+            if (currentRocketIdx < 0)
+            {
+                currentRocketIdx = 0;
+            }
+            SetCurrentRocketIndex(currentRocketIdx);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentRocketIdx++;
+            if (currentRocketIdx > rocketSettings.Count)
+            {
+                currentRocketIdx = rocketSettings.Count - 1;
+            }
+            SetCurrentRocketIndex(currentRocketIdx);
+        }
     }
+
+    private void HandleAudioChanges()
+    {
+        if (playAudio && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+            audioSource.mute = false;
+        }
+
+        if (!playAudio && audioSource.isPlaying)
+        {
+            audioSource.mute = true;
+            audioSource.Stop();
+        }
+    }
+
+    private void HandleThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // using rocket's coordinate system
+            rigidBody.AddRelativeForce(Vector3.up * boostForce * Time.deltaTime);
+            playAudio = true;
+        }
+        else
+        {
+            playAudio = false;
+        }
+    }
+
+    private void HandleRotationInput()
+    {
+        rigidBody.freezeRotation = true; // start manual rotation control
+
+        // left rotate has precedence 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationForce * Time.deltaTime);
+        }
+
+        rigidBody.freezeRotation = false; // resume physics rotation control
+    }
+
 
 }
