cd1f23d8964f61d87030e64e26607f6b95b9bccc
ellielove <ellie@pacifist.studio>
2020-12-06 14:07:12 -0800

    Camera Controller
    just follows target, without rotation

---
+++ 3_boost/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform followTarget;
+
+    [SerializeField]
+    Vector3 offset;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        this.transform.position = followTarget.position + offset;
+	}
+}
