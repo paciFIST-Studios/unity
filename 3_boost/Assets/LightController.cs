252f6e0089e870df339f967f73939295b72dbfad
ellielove <ellie@pacifist.studio>
2020-12-06 14:06:49 -0800

    Light Controller
    1. toggles materials when collision enter/exit with object marked as Player

---
+++ 3_boost/Assets/LightController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField]
+    private Material empty;
+
+    [SerializeField]
+    private Material occupied;
+    
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = this.GetComponent<MeshRenderer>();    
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            meshRenderer.material = occupied;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            meshRenderer.material = empty;
+        }
+    }
+}
