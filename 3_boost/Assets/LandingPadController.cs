f226a8df54cff35b4a5b03c3fbb9e1877bddf6be
ellielove <ellie@pacifist.studio>
2020-12-08 22:24:01 -0800

    Landing Pad Controller
    1. Added prefab for landing pad, that is adjustible in size, draws in the editor, and shows which specific chunk you landed on, allowing for use to use some kind of "Bullseye" mechanic in the future
    
    2. You have to write in the tag you want to use in the script-vars, in editor, but it looks like it works okay

---
+++ 3_boost/Assets/LandingPadController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingPadController : MonoBehaviour
+{
+    [SerializeField] GameObject targetPrefab;
+    [SerializeField] Vector2 dimensions;
+    [SerializeField] string TagLaunchpadAs;
+    private List<GameObject> landingPadSegments;
+
+    bool drawGizmo = true;
+
+    private void Start()
+    {
+        drawGizmo = false;
+
+        Vector3 centerOfTopLeft = transform.position;
+        float halfSizeOfUnit = targetPrefab.transform.lossyScale.x * 0.5f;
+        centerOfTopLeft.x += -(dimensions.x * 0.5f) + halfSizeOfUnit;
+        centerOfTopLeft.z += -(dimensions.y * 0.5f) + halfSizeOfUnit;
+
+        int totalBlocks = (int)Mathf.Ceil(dimensions.x * dimensions.y);
+        landingPadSegments = new List<GameObject>(totalBlocks);
+
+        for(int i = 0; i < dimensions.x; i++)
+        {
+            for(int j = 0; j < dimensions.y; j++)
+            {
+                var obj = Instantiate(targetPrefab);
+                obj.tag = TagLaunchpadAs;
+                Vector3 newPos = centerOfTopLeft + new Vector3(i, 0, j);
+                obj.transform.SetPositionAndRotation(newPos, Quaternion.identity);
+                obj.transform.SetParent(this.transform);
+                landingPadSegments.Add(obj);
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(!drawGizmo) { return; }
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawCube(
+              transform.position
+            , new Vector3(dimensions.x, 1, dimensions.y)
+            );
+    }
+
+}
