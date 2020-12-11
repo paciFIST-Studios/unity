0c86f5aee6c67bb3e9be70c79468243c51a72476
ellielove <ellie@pacifist.studio>
2020-12-10 23:19:25 -0800

    Add Main Menu, Font, and misc

---
+++ 3_boost/Assets/scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+    public void StartNewGame()
+    {
+        // the main menu is scene 0
+        SceneManager.LoadScene(1);
+    }
+
+    public void ContinueGame() { }
+
+    public void Settings() { }
+
+    public void About() { }
+
+    public void ExitProgram()
+    {
+        // todo: ellie: save data
+        Application.Quit();
+    }
+
+
+
+}
