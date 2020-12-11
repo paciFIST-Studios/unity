bb44518089e352da6816b713c3bbd3baf8152af9
ellielove <ellie@pacifist.studio>
2020-12-11 13:21:25 -0800

    Update Nav Mesh Navigation

---
+++ _combat_rpg/combat rpg/Assets/Scripts/NavAgentMovementController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentMovementController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        this.GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
