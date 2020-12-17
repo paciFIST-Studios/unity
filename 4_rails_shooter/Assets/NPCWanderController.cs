de9ec96a1ae7ba87a3329e202824af070d04daf4
ellielove <ellie@pacifist.studio>
2020-12-17 07:46:11 -0800

    NPC Wander Controller
    This is based on oscilation controller, but I wanted to separate them, so we could start adding things like: turn the object around 180 deg, when switching direction

---
+++ 4_rails_shooter/Assets/NPCWanderController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCWanderController : MonoBehaviour
+{
+    private float hertz;
+    private Vector3 startPosition;
+
+    [Header("Movement")]
+    [SerializeField] private Vector3 offsetVector;
+    [SerializeField] [Range(0, 1)] private float movementFactor;
+    [SerializeField] private float oscillationPeriod = 1f;
+    [SerializeField] private bool showOffsetEndpoint;
+    [SerializeField] private GameObject offsetEndpointMarkerPrefab;
+    private GameObject offsetEndpointMarker;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+
+        offsetEndpointMarker = Instantiate(offsetEndpointMarkerPrefab);
+        offsetEndpointMarker.GetComponent<MeshRenderer>().enabled = false;
+
+
+        if (float.IsNaN(oscillationPeriod))
+        {
+            hertz = 1;
+        }
+        else
+        {
+            hertz = 1 / oscillationPeriod;
+        }
+    }
+
+    private void Update()
+    {
+        if (offsetEndpointMarker)
+        {
+            offsetEndpointMarker.GetComponent<MeshRenderer>().enabled = showOffsetEndpoint;
+            offsetEndpointMarker.transform.position = startPosition + offsetVector;
+        }
+
+        float cycles = Time.time * hertz;
+
+        const float tau = Mathf.PI * 2;
+        float rawSin = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSin * 0.5f) + 0.5f;
+
+        Vector3 offset = offsetVector * movementFactor;
+        transform.position = startPosition + offset * rawSin;
+    }
+}
