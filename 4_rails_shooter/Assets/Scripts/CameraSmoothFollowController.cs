fd7950c3141c733a92e1ef67adfd643a69938d25
ellielove <ellie@pacifist.studio>
2020-12-17 01:19:20 -0800

    Update Camera Controller
    1. rename file
    
    2. added smooth movement, and quality of life things

---
+++ 4_rails_shooter/Assets/Scripts/CameraSmoothFollowController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct CameraSmoothFollowPosition
+{ 
+    [SerializeField] public Vector3 followTargetOffsetPosition;
+    [SerializeField] public Vector3 cameraOffsetPosition;
+}
+
+public class CameraSmoothFollowController : MonoBehaviour
+{
+
+    [Header("Follow Target Settings")]
+    // a reference to the target, which the camera is meant to follow
+    [SerializeField] public Transform followTarget;
+    // an offset, which changes the "lead" the camera has on the follow target
+    [SerializeField] private Vector3 followTargetOffsetPosition;
+    [SerializeField] private bool markFollowTargetPosition;
+
+    [SerializeField] private GameObject followTargetMarkerPrefab;
+    private GameObject followTargetMarker;
+
+    [Header("Camera Settings")]
+    // an offset, which changes the camera's position
+    [SerializeField] private Vector3 cameraOffsetPosition;
+    // translation speed is how fast the camera moves
+    [SerializeField] private float cameraTranslationSpeed = 10f;
+    // rotation speed is how fast the camera turns
+    [SerializeField] private float cameraRotationSpeed = 10f;
+
+    // named, preset positions the camera may be placed at, around the follow target
+    [SerializeField] private List<CameraSmoothFollowPosition> presetPositions;
+
+    [Header("Smoothing")]
+    // lerp smoothing is applied to translational movement
+    [SerializeField] private float lerpSmoothing = 0.1f;
+    // slerp smoothing is applied to rotational movement
+    [SerializeField] private float slerpSmoothing = 0.1f;
+
+    private void Start()
+    {
+        followTargetMarker = Instantiate(followTargetMarkerPrefab);
+        followTargetMarker.transform.SetParent(transform);
+    }
+
+    // camera is done in late update, so it takes place after all of the user input
+    // is collected, and after all of the movement has been applied by physics
+    private void LateUpdate()
+    {
+
+        if (followTarget)
+        {
+            // branchless code, which takes care of the follow marker.  It's childed to the camera
+            {
+                followTargetMarker.GetComponent<MeshRenderer>().enabled = markFollowTargetPosition;
+                followTargetMarker.transform.position = followTarget.position + followTargetOffsetPosition;
+            }
+
+            Vector3 lookPosition = followTarget.position + followTargetOffsetPosition;
+            Vector3 relativePos = lookPosition - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+            transform.rotation = Quaternion.Slerp(
+                  transform.rotation
+                , rotation
+                , Time.deltaTime * cameraRotationSpeed * slerpSmoothing
+                );
+
+            Vector3 cameraDesiredPosition = 
+                  followTarget.transform.position 
+                  // we use a minus here for convenience sake in-editor.  
+                  // A positive value represents "distance away from" the target.
+                  // we *could* cange this to a plus, and then provide a -dist, to show
+                  // how far behind the camera should follow, but this saves a keystroke
+                - cameraOffsetPosition.x * followTarget.transform.forward
+                + cameraOffsetPosition.y * followTarget.transform.up
+                + cameraOffsetPosition.z * followTarget.transform.right
+                ;
+
+            this.transform.position = Vector3.Lerp(
+                  transform.position
+                , cameraDesiredPosition
+                , Time.deltaTime * cameraTranslationSpeed * lerpSmoothing
+                );
+        }
+    }
+}
