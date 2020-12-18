f97f7a6589f2587d5743ffb2f6c959c3c415a89b
ellielove <ellie@pacifist.studio>
2020-12-17 19:12:30 -0800

    Update
    1. updated / refactored player boat controller
    1.a. clearer movement handling
    1.b. clearer y-axis constraints for boat
    
    2. scene update

--- 4_rails_shooter/Assets/Scripts/PlayerBoatController.cs
+++ 4_rails_shooter/Assets/Scripts/PlayerBoatController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[System.Serializable]
+public struct ClampVec2
+{
+    [SerializeField] public float min;
+    [SerializeField] public float max;
+}
+
 public class PlayerBoatController : MonoBehaviour
 {
     [Header("Player Stats")]
-    [SerializeField] float lateralMoveSpeed = 1f;
-    [SerializeField] float rotationSpeed = 1f;
+    [Tooltip("Movement speed of the player, to be applied in local space")]
+    [SerializeField] Vector3 movementSpeedVector;
     [SerializeField] float debugSprintMultiplier = 20f;
 
+    [Tooltip("DO NOT USE.  This is the current value used for rotation.  To change rotation, change either controller rotation, or mouse rotation")]
+    [SerializeField] private float currentRotationSpeed = 1f;
     [SerializeField] private float controllerRotationSpeed = 200f;
     [SerializeField] private float mouseRotationSpeed = 5f;
 
     [SerializeField] private float xAxisClampMag;
+    [SerializeField] private ClampVec2 yAxisClampHeight;
     [SerializeField] private float fixedPlayerDepth = 20f;
 
     bool isMoving = false;
@@ -44,7 +54,7 @@ public class PlayerBoatController : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)
+        if (ctx.started)
         {
             isMoving = true;
         }
@@ -53,6 +63,7 @@ public class PlayerBoatController : MonoBehaviour
             isMoving = false;
             return;
         }
+        // else if started
 
         print("OnMove() : " + ctx.action.name);
 
@@ -62,7 +73,7 @@ public class PlayerBoatController : MonoBehaviour
 
     public void OnLook(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)
+        if (ctx.started)
         {
             isRotating = true;
         }
@@ -75,34 +86,46 @@ public class PlayerBoatController : MonoBehaviour
         var lookVector = ctx.ReadValue<Vector2>();
         rotationThisTick = lookVector.x;
 
-
+        // check to see if input was from mouse delta
         if (ctx.action.GetBindingDisplayString() == "Delta")
         {
-            rotationSpeed = mouseRotationSpeed;
+            currentRotationSpeed = mouseRotationSpeed;
         }
         else
         {
-            rotationSpeed = controllerRotationSpeed;
+            currentRotationSpeed = controllerRotationSpeed;
         }
     }
 
 
     private void MoveCharacter(Vector2 vec)
     {
-        Vector3 moveVector = transform.right * vec.x;
+        var displacementVector = Vector3.zero;
+        displacementVector += vec.x * transform.right;
+        displacementVector += vec.y * transform.up;         // y as up-down
+      //displacementVector += vec.y * transform.forward;    // y as depth
 
-        // moveVector += transform.up * vec.y       // y as up-down
-        // moveVector += transform.forward * vec.y  // y as depth
 
         if (Keyboard.current.leftShiftKey.isPressed)
         {
-            moveVector *= debugSprintMultiplier;
+            displacementVector *= debugSprintMultiplier;
         }
 
-        var currentPos = transform.localPosition;
-        currentPos += Time.deltaTime * moveVector * lateralMoveSpeed;
+        Vector3 currentPos = transform.localPosition;
+
+        // change current position, according to each component
+        // each component, is increased by the displacement vector component
+        //           , scaled by the corresponding movement vector component
+        // all of these are also scaled by dt
+        currentPos.x += Time.deltaTime * displacementVector.x * movementSpeedVector.x;
+        currentPos.y += Time.deltaTime * displacementVector.y * movementSpeedVector.y;
+        currentPos.z += Time.deltaTime * displacementVector.z * movementSpeedVector.z;
+
+        // clamp resulting change to acceptable boundary
         currentPos.x = Mathf.Clamp(currentPos.x, -xAxisClampMag, xAxisClampMag);
+        currentPos.y = Mathf.Clamp(currentPos.y, yAxisClampHeight.min, yAxisClampHeight.max);
         currentPos.z = fixedPlayerDepth;
+
         transform.localPosition = currentPos;
     }
 
@@ -110,7 +133,7 @@ public class PlayerBoatController : MonoBehaviour
     private void RotateCharacter(float degrees)
     {
         var ro = transform.localRotation.eulerAngles;
-        ro.y += degrees * rotationSpeed * Time.deltaTime;
+        ro.y += degrees * currentRotationSpeed * Time.deltaTime;
         transform.localRotation = Quaternion.Euler(ro.x, ro.y, ro.z);
     }
 
