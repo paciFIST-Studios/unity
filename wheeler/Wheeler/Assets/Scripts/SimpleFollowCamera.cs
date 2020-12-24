f898b0254d059fbaa0082804c358ee2e44c5a89a
ellielove <ellie@pacifist.studio>
2020-12-23 22:59:56 -0800

    Follow camera script

---
+++ wheeler/Wheeler/Assets/Scripts/SimpleFollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollowCamera : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+
+    [SerializeField] private Transform target;
+
+    private void Update()
+    {
+        transform.position = target.position + offset;
+    }
+
+}
