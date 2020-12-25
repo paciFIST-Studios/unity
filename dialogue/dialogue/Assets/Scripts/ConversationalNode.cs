da66d57c14575710a8c42db7fbff10a486f501dd
ellielove <ellie@pacifist.studio>
2020-12-25 11:20:23 -0800

    update

---
+++ dialogue/dialogue/Assets/Scripts/ConversationalNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PrimalEmotions
+{
+      Seeking
+    , Rage
+    , Fear
+    , Lust
+    , Care
+    , Panic
+    , Play
+}
+
+public enum AffectiveFeelings
+{
+      Enthusiastic
+    , PissedOff
+    , Anxious
+    , Horny
+    , TenderAndLoving
+    , LonelyAndSad
+    , Joyous
+}
+
+
+public class ConversationalNode : MonoBehaviour
+{
+    public string nodeName;
+    public int nodeID;
+    public string nodeDesc;
+    public Vector3 v;
+
+
+}
