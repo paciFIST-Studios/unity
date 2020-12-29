dd37750bf09623000da55b72944cb9c197b03d52
ellielove <ellie@pacifist.studio>
2020-12-28 18:55:39 -0800

    Added surface langer and stubbed in its door controller

---
+++ wheeler/Wheeler/Assets/Scripts/SurfaceLanderController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceLanderController : MonoBehaviour
+{
+    [SerializeField] private Transform doorHinge;
+    [SerializeField] [Range(0, 1)] private float doorActivation;
+
+    private Vector3 doorRotationAxis;
+
+    private void Start()
+    {
+        // take transform of door, and then provide an offset from its centerpoint, to a point on its rotation axis
+        // 
+
+    }
+
+
+}
