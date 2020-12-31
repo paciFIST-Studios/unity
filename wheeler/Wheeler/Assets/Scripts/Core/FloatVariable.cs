69b71f70b8cc4b19958b2f8e49f48818ebf07aa8
ellielove <ellie@pacifist.studio>
2020-12-30 19:03:44 -0800

    Scriptable Object Update: 1
    1. added GenericVariable and FloatVariable
    
    2. FloatVariable : GenericVariable<Float>
    
    3. The goal of these classes, is to use scriptable objects, so the data exists in engine, and can be referenced by other classes, without having to resort to big singleton classes
    
    This change is is preparation for the dialogue system, which was using big singleton classes.
    
    The reasons for starting to use scriptable objects this way, are:
    a. https://www.youtube.com/watch?v=raQ3iHhE_Kk
    b. https://www.youtube.com/watch?v=6vmRwLYWNRo

---
+++ wheeler/Wheeler/Assets/Scripts/Core/FloatVariable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using pacifist.core;
+
+[CreateAssetMenu(fileName = "New Float Variable", menuName = "paciFIST/Core/FloatVariable")]
+public class FloatVariable : GenericVariable<float>
+{
+    // ctors
+    public FloatVariable() { this.value = 0f; }
+    public FloatVariable(float f) { this.value = f; }
+    public FloatVariable(FloatVariable fv) { this.value = fv.value; }
+
+    // modifiers
+    public void SetValue(FloatVariable fv)
+    {
+        this.value = fv.value;
+    }
+
+    public void ApplyChange(float amount)
+    {
+        this.value += amount;
+    }
+
+    public void ApplyChange(FloatVariable amount)
+    {
+        this.value += amount.value;
+    }
+
+}
---
+++ wheeler/Wheeler/Assets/Scripts/Core/GenericVariable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace pacifist
+{
+    namespace core
+    {
+        // https://github.com/roboryantron/Unite2017/blob/master/Assets/Code/Variables/FloatVariable.cs
+
+
+        public abstract class GenericVariable<T> : ScriptableObject
+        {
+#if UNITY_EDITOR
+            [Multiline]
+            public string DeveloperDescription = "";
+#endif
+
+            public T value;
+
+            public T GetValue()
+            {
+                return value;
+            }
+
+            public void SetValue(T val)
+            {
+                value = val;
+            }
+        }
+
+
+
+
+
+    }
+}
