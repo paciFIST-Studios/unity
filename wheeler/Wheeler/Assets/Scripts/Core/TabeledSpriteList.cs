7c8607ec1e56f6cf1431b21c0e0c88e3940b19d5
ellielove <ellie@pacifist.studio>
2021-01-01 13:55:45 -0800

    Added a scriptable object containing List<SpriteVariable>
    This is a template.
    
    TheElements and TheFactions both use this template as their framework

---
+++ wheeler/Wheeler/Assets/Scripts/Core/TabeledSpriteList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(menuName = "paciFIST/TabeledSpriteList")]
+public class TabeledSpriteList : ScriptableObject
+{
+    [TableList]
+    [SerializeField]
+    List<SpriteVariable> TheElements = new List<SpriteVariable>();
+}
