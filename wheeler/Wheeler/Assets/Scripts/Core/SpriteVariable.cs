28311ac7d2514c91cb4558169c92fb35e49f7153
ellielove <ellie@pacifist.studio>
2020-12-31 23:44:36 -0800

    Adding Sprite References
    1. Adding SpriteReference
    2. Adding SpriteVariable
    
    3. Adding a bunch of sprites

---
+++ wheeler/Wheeler/Assets/Scripts/Core/SpriteVariable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using pacifist.core;
+
+[CreateAssetMenu(fileName = "New Sprite Variable", menuName = "paciFIST/SpriteVariable")]
+public class SpriteVariable : GenericVariable<Sprite>
+{
+    public SpriteVariable() { }
+    public SpriteVariable(Sprite s) { this.value = s; }
+
+    public new void SetValue(Sprite s)
+    {
+        this.value = s;
+    }
+
+    public static implicit operator Sprite(SpriteVariable s)
+    {
+        return s.value;
+    }
+
+}
