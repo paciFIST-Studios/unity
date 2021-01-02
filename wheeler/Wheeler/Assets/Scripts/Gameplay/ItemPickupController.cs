ff83d8585ec8ef533ac4c9799bd7cd304e1e12c9
ellielove <ellie@pacifist.studio>
2021-01-01 19:12:58 -0800

    ItemPickupController
    1. small script that destroys item after player trigger enter

---
+++ wheeler/Wheeler/Assets/Scripts/Gameplay/ItemPickupController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+public class ItemPickupController : MonoBehaviour
+{
+    [OnValueChanged("LoadData")]
+    [InlineEditor]
+    public InventoryItem data;
+    
+    private void LoadData(InventoryItem data)
+    {
+        print("ItemLoaded: " + data.name);
+    }
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<WheelerPlayerController>() == null) { return; }
+
+        if(data)
+        {
+            print("Pickup: " + data.GetCurrentResearchLevelDisplayName());
+            data = null;
+            Destroy(this.gameObject);
+        }       
+    }
+}
