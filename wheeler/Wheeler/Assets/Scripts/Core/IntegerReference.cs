643ef28bdb0963500ec77b044571706b51fe0aab
ellielove <ellie@pacifist.studio>
2021-01-01 16:01:38 -0800

    Added IntegerVariable and IntegerReference

---
+++ wheeler/Wheeler/Assets/Scripts/Core/IntegerReference.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using UnityEngine;
+
+using pacifist.core;
+
+using Sirenix.OdinInspector;
+
+[Serializable]
+public class IntegerReference : GenericVariableReference<int>
+{
+    [FoldoutGroup("$GuiTitle")]
+    [HorizontalGroup("$GuiTitle/Column/Bottom")]
+    [OnStateUpdate("UpdateReference")]
+    [LabelWidth(85)]
+    public new IntegerVariable Reference;
+
+    public IntegerReference()
+    {
+        this.GuiTitle = "IntegerReference";
+        UpdateReference();
+    }
+
+    public IntegerReference(int i)
+    {
+        this.UseOverride = true;
+        this.OverrideValue = i;
+        this.GuiTitle = "IntegerReference";
+
+        UpdateReference();
+    }
+
+    public int Value { get { return UseOverride ? OverrideValue : Reference.value; } }
+    public static implicit operator int(IntegerReference ir) { return ir.Value; }
+
+
+    public override void UpdateReference()
+    {
+        if (Reference)
+        {
+            this.GuiTitle = this.Reference.DisplayName;
+            this.ReferencedValue = this.Reference.value;
+        }
+    }
+    public override void SaveOverrideToReference()
+    {
+        if (Reference)
+        {
+            this.Reference.SetValue(OverrideValue);
+            UpdateReference();
+        }
+    }
+}
---
+++ wheeler/Wheeler/Assets/Scripts/Core/IntegerVariable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+using pacifist.core;
+
+[CreateAssetMenu(fileName = "New Integer Variable", menuName = "paciFIST/IntegerVariable")]
+public class IntegerVariable : GenericVariable<int>
+{
+    public IntegerVariable() { this.value = 0; }
+    public IntegerVariable(int i) { this.value = i; }
+    public IntegerVariable(IntegerVariable iv) { this.value = iv.value; }
+    public void SetValue(IntegerVariable iv) { this.value = iv.value; }
+    public static implicit operator int(IntegerVariable iv) { return iv.value; }
+}
