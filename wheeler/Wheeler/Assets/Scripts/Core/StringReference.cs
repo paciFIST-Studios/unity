12789fea49cc41b369e39197427b323a9b3e9732
ellielove <ellie@pacifist.studio>
2021-01-01 16:04:18 -0800

    Added StringVariable and StringReference

---
+++ wheeler/Wheeler/Assets/Scripts/Core/StringReference.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using UnityEngine;
+
+using pacifist.core;
+
+using Sirenix.OdinInspector;
+
+[Serializable]
+public class StringReference : GenericVariableReference<string>
+{
+    [FoldoutGroup("$GuiTitle")]
+    [HorizontalGroup("$GuiTitle/Column/Bottom")]
+    [OnStateUpdate("UpdateReference")]
+    [LabelWidth(85)]
+    public new StringVariable Reference;
+
+    public StringReference()
+    {
+        this.GuiTitle = "StringReference";
+        UpdateReference();
+    }
+
+    public StringReference(string s)
+    {
+        this.UseOverride = true;
+        this.OverrideValue = s;
+        this.GuiTitle = "StringReference";
+        UpdateReference();
+    }
+
+    public string Value
+    {
+        get { return UseOverride ? OverrideValue : Reference.value; }
+    }
+
+    public override void UpdateReference()
+    {
+        if (Reference)
+        {
+            this.GuiTitle = this.Reference.DisplayName;
+            this.ReferencedValue = this.Reference.value;
+        }
+    }
+
+    public override void SaveOverrideToReference()
+    {
+        if (Reference)
+        {
+            this.Reference.SetValue(OverrideValue);
+            UpdateReference();
+        }
+    }
+}
---
+++ wheeler/Wheeler/Assets/Scripts/Core/StringVariable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+using pacifist.core;
+
+[CreateAssetMenu(fileName = "New String Variable", menuName = "paciFIST/StringVariable")]
+public class StringVariable : GenericVariable<string>
+{
+    public StringVariable() { this.value = string.Empty; }
+    public StringVariable(string s) { this.value = s; }
+    public StringVariable(StringVariable sv) { this.value = sv.value; }
+    public void SetValue(StringVariable sv) { this.value = sv.value; }
+    public static implicit operator string(StringVariable sv) { return sv.value; }
+}
