da4f2a4bf67c3e763b84bc64af6161ea1e9232dc
ellielove <ellie@pacifist.studio>
2021-01-01 19:15:00 -0800

    Update InventoryItem.cs

--- wheeler/Wheeler/Assets/Scripts/Gameplay/InventoryItem.cs
+++ wheeler/Wheeler/Assets/Scripts/Gameplay/InventoryItem.cs
@@ -1,15 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-using pacifist.core;
+﻿using UnityEngine;
 
 using Sirenix.OdinInspector;
 
 [CreateAssetMenu(fileName = "New Inventory Item", menuName = "paciFIST/InventoryItem")]
 public class InventoryItem : ScriptableObject
 {
-    [BoxGroup][HideLabel]
+    [BoxGroup] [HideLabel]
     public SpriteReference sprite;
 
     // increasing research level increments the display name
@@ -21,4 +17,21 @@ public class InventoryItem : ScriptableObject
 
     public float ItemExchangeValue;
 
+
+    public string GetCurrentResearchLevelDisplayName()
+    {
+        if(ResearchableNames.Length == 0)
+        {
+            return "ERROR: NO Researchable Names PRESENT";
+        }
+
+        if(ResearchableDescriptions.Length == 0)
+        {
+            return "ERROR: NO Researchable Descriptions PRESENT";
+        }
+
+        return (ResearchLevel > ResearchableNames.Length - 1) 
+            ? ResearchableNames[ResearchableNames.Length - 1].value 
+            : ResearchableNames[ResearchLevel].value;
+    }
 }
