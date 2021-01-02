fea20ceb7b26dd6cb770d51d3a2da06a446ff9e7
ellielove <ellie@pacifist.studio>
2021-01-01 16:03:40 -0800

    Added some basic inventory items

---
+++ wheeler/Wheeler/Assets/Scripts/Gameplay/InventoryItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using pacifist.core;
+
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "New Inventory Item", menuName = "paciFIST/InventoryItem")]
+public class InventoryItem : ScriptableObject
+{
+    [BoxGroup][HideLabel]
+    public SpriteReference sprite;
+
+    // increasing research level increments the display name
+    public StringReference[] ResearchableNames;
+    public StringReference[] ResearchableDescriptions;
+
+    [Range(0, 5)]
+    public int ResearchLevel;
+
+    public float ItemExchangeValue;
+
+}
