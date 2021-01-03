e02ef068929bebc26aa4b2421d19e8853fb80c5f
ellielove <ellie@pacifist.studio>
2021-01-02 16:26:09 -0800

    Add start menu

---
+++ wheeler/Wheeler/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Sirenix.OdinInspector;
+
+[System.Serializable]
+public class MainMenuController : MonoBehaviour
+{
+    [BoxGroup("UI Elements")]
+    [LabelWidth(120)]
+    [SerializeField] Transform backgroundImage;
+
+    [BoxGroup("UI Elements")]
+    [LabelWidth(120)]
+    [SerializeField] Transform titleText;
+
+    [BoxGroup("UI Elements")]
+    [LabelWidth(120)]
+    [SerializeField] Transform startButton;
+
+    [BoxGroup("UI Elements")]
+    [LabelWidth(120)]
+    [SerializeField] Transform settingsButton;
+
+    [BoxGroup("UI Elements")]
+    [LabelWidth(120)]
+    [SerializeField] Transform buildInfoButton;
+         
+    [BoxGroup("UI Elements")]
+    [LabelWidth(120)]
+    [SerializeField] Transform exitButton;
+
+
+
+
+    public void OnStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnSettings()
+    {
+
+    }
+
+    public void OnBuildInfo()
+    {
+
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+
+}
