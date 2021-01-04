f4cdfe07d12406ca254d1efde236bcf9328aa175
ellielove <ellie@pacifist.studio>
2021-01-04 12:41:31 -0800

    Adding support for scannable flavors
    1. MaterialVariable.cs
    
    A basic class, similar to GenericVariable<T>.  Basically though, for any Unity class, we're just going to roll a new one and inherit from ScriptableObject, rather than try and inherit from GenericVariable<T>, b/c of how OdinInspector works for specifying the serialziation approach of images
    
    2. TabeledMaterialList.cs  - just used for material enums // todo: consider rename?
    
    3. Materials
    
    these already exist in the project, but now they're stored in MaterialVariable,s so we can reference them more easily
    
    4. ScannableFlavorsEnum - this is is just the asset that holds our enum

---
+++ wheeler/Wheeler/Assets/Scripts/Core/MaterialVariable.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "New Material Variable", menuName = "paciFIST/MaterialVariable")]
+public class MaterialVariable : ScriptableObject
+{
+    [TableColumnWidth(60)]
+    [PreviewField(50, ObjectFieldAlignment.Center)]
+    public Material Value;
+
+    [TableColumnWidth(100)]
+    [LabelWidth(100)]
+    public string DevName;
+
+    [TableColumnWidth(200)]
+    [MultiLineProperty]
+    public string Description;
+
+    public MaterialVariable() { }
+    public MaterialVariable(Material m) { this.Value = m; }
+
+    public void SetValue(Material m)
+    {
+        this.Value = m;
+    }
+
+    public static implicit operator Material(MaterialVariable m)
+    {
+        return m.Value;
+    }
+
+}
---
+++ wheeler/Wheeler/Assets/Scripts/Core/TabeledMaterialList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "Tabled Material List", menuName = "paciFIST/TabledMaterialList")]
+public class TabeledMaterialList : ScriptableObject
+{
+    [TableList][SerializeField]
+    List<MaterialVariable> materials = new List<MaterialVariable>();
+}
