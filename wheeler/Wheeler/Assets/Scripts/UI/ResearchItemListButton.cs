668c5181d6cebe729cfa573090374a106409838c
ellielove <ellie@pacifist.studio>
2021-01-08 01:56:26 -0800

    Code for managing research inventory

---
+++ wheeler/Wheeler/Assets/Scripts/UI/ResearchItemListButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ResearchItemListButton : MonoBehaviour
+{
+    [SerializeField]
+    private Text label;
+
+    public void SetText(string text)
+    {
+        label.text = text;
+    }
+
+    public void OnClick()
+    {
+        print(string.Format($"Button: {0}", label.text));
+    }
+
+}
---
+++ wheeler/Wheeler/Assets/Scripts/UI/ResearchListController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchListController : MonoBehaviour
+{
+    [SerializeField] private GameObject buttonPrefab;
+
+    private List<string> inventory = new List<string>();
+
+    private void CreateButton(string name)
+    {
+        var button = (GameObject)Instantiate(buttonPrefab);
+        button.SetActive(true);
+        button.GetComponent<ResearchItemListButton>().SetText(name);
+        button.transform.SetParent(buttonPrefab.transform.parent, false);
+    }
+
+    public void AddInventoryItem(InventoryItem item)
+    {
+        inventory.Add(item.name);
+        CreateButton(item.name);
+    }
+
+    public void SetInventory(InventoryItem[] inventory)
+    {
+        this.inventory = new List<string>(inventory.Length);
+
+        for(int i = 0; i < inventory.Length; i++)
+        {
+            this.inventory.Add(inventory[i].name);
+        }
+
+        //StartCoroutine(RebuildInventoryAsync());
+
+        RebuildInventory();
+    }
+
+    IEnumerator RebuildInventoryAsync()
+    {
+        print("Rebuild Inventory Async");
+        for(int i = 0; i < this.inventory.Count; i++)
+        {
+            CreateButton(inventory[i]);
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return null;
+    }
+
+    public void RebuildInventory()
+    {
+        print("Rebuild Inventory Sync");
+        for(int i = 0; i < this.inventory.Count; i++)
+        {
+            CreateButton(inventory[i]);
+        }
+    }
+
+}
