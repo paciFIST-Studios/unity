9e72132ff25feaf890c1ae91ad83c15c079216cd
paciFIST-Studios <ellie@pacifist.studio>
2021-01-28 21:56:04 -0800

    adding generic invokable skill
    Going with the plan of having an upgrade tree, so sketched out a generic invokable skill

---
+++ wheeler/Wheeler/Assets/Scripts/Gameplay/TriggerableSkill.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Triggerable Skill", menuName = "paciFIST/TriggerableSkill")]
+public class TriggerableSkill : ScriptableObject
+{
+    // upgrade icon
+    // equipped icon (if can be equipped)
+    // invokable payload?
+        // handles cooldown?
+
+
+    public TriggerableSkillPayloadBase payload;
+}
