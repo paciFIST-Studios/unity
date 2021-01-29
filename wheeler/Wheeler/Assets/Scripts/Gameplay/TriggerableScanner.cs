0e2a380db8a9489c11c6ad1fc23469bd9df10866
paciFIST-Studios <ellie@pacifist.studio>
2021-01-28 21:57:25 -0800

    stub in triggerable skills

---
+++ wheeler/Wheeler/Assets/Scripts/Gameplay/TriggerableScanner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Triggerable Scanner", menuName = "paciFIST/TriggerableScanner")]
+public class TriggerableScanner : TriggerableSkillPayloadBase
+{
+    public string name;
+    public float cooldown;
+
+    public ParticleSystem particleSystem;
+
+    private float lastUsed;
+
+
+    // name or enum
+
+    // last time fired
+    // cooldown
+    // particle system
+
+    public override void Invoke()
+    {
+        // if cooldown elapsed
+            // fire
+    }
+}
