6d54097aaeae0adf9e0676f2ae5cfdadbde4e2d9
paciFIST-Studios <ellie@pacifist.studio>
2021-02-03 16:49:08 -0800

    stub

---
+++ wheeler/Wheeler/Assets/Scripts/Dialogue/TriggerableDialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerableDialogue : MonoBehaviour
+{
+    [SerializeField] private DialogueWindowManager manager;
+
+    [SerializeField] private DialogueConversation conversation;
+
+
+    public void OnTriggerStay(Collider other)
+    {
+        // if other is player
+            // and if trigger button is pressed
+    }
+
+
+
+    public void TriggerDialogue()
+    {
+        // do this as a coroutine?
+        manager.StartConversation(conversation);
+    }
+}
