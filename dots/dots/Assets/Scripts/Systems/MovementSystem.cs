ff8b6b6d5c0d0dbb4925fd32bedb535051fe9632
paciFIST-Studios <ellie@pacifist.studio>
2021-02-11 22:31:45 -0800

    Movement ECS
    Moves an entity up and down, and reflects it back at +/- 5

---
+++ dots/dots/Assets/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MovementSystem : ComponentSystem
+{
+
+    protected override void OnUpdate()
+    {
+        // foreach
+        Entities.ForEach((ref Translation translation, ref MovementSpeedComponent movementSpeedComponent) => {
+            translation.Value.y += movementSpeedComponent.moveSpeed * Time.DeltaTime;
+
+            if(translation.Value.y > 5f) {
+                movementSpeedComponent.moveSpeed = -Mathf.Abs(movementSpeedComponent.moveSpeed);
+            }
+
+            if (translation.Value.y < -5f){
+                movementSpeedComponent.moveSpeed = +Mathf.Abs(movementSpeedComponent.moveSpeed);
+            }
+
+
+
+        });
+
+
+    }
+}
