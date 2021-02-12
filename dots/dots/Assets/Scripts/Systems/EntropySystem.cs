307fde22c89b53844982ae0903604122fe10532c
paciFIST-Studios <ellie@pacifist.studio>
2021-02-11 22:30:51 -0800

    Entropy ECS
    encapsulates the concept of degradation over time.  Used for maintenance or hunger?

---
+++ dots/dots/Assets/Scripts/Systems/EntropySystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+
+public class EntropySystem : ComponentSystem
+{
+    // degredation per second
+    public float degradePS = 1f;
+
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref EntropyComponent entropyComponent) =>
+        {
+            entropyComponent.value -= degradePS * Time.DeltaTime;
+        });
+    }
+}
