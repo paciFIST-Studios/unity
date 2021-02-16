af0e8d5654f94646d2fb8788e6431ea13005b0e5
paciFIST-Studios <ellie@pacifist.studio>
2021-02-16 00:54:11 -0800

    ScriptableObject: WheelerPlayerCharacterMenuPanel
    1. added class for this, it's just two references
    
    2. added the objects to the project

---
+++ wheeler/Wheeler/Assets/Scripts/UI/WheelerPlayerCharacterMenuPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "New Character Menu Panel", menuName = "paciFIST/ui/CharacterMenuPanel")]
+public class WheelerPlayerCharacterMenuPanel : ScriptableObject
+{
+    [SerializeField][Required] public RectTransform header;
+    [SerializeField][Required] public RectTransform body;
+}
